e578543ceda0836035e8477737accdbd7aa6c61b
Gabriel Dias Faria <36157517+GabrielDiasFaria@users.noreply.github.com>
2025-03-19 07:31:43 +0000

    Create page.tsx

---
+++ app/artifacts/page.tsx
@@ -0,0 +1,9 @@
+const ArtifactsPage = () => {
+  return (
+    <div className="flex flex-col w-full h-full bg-theme-background">
+      Artefatos
+    </div>
+  );
+};
+
+export default ArtifactsPage;
