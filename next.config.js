681640a4c5f37ba3aea2e2f2ac70fcc1f7453e65
Gabriel Dias Faria <36157517+GabrielDiasFaria@users.noreply.github.com>
2025-03-19 07:29:08 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: "/sap/bc/ui5_ui5/sap/zz1next/static",
+  trailingSlash: true,
+  output: "export"
+};
+
+module.exports = nextConfig;
